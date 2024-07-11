26451403c1846b7617ed8470b1fe1a96d1992767
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-11 09:01:16 -0400

    Setup header region

--- components/chatbot/components/header/header.jsx
+++ components/chatbot/components/header/header.jsx
@@ -1,5 +1,22 @@
+// ============================================================================
+// Chatbot Header
+// ============================================================================
+
 // Global header component
 // Supports:
 // - Brand name/logo
 // - Menu
 // - Options
+
+// Import styles
+import './Header.scss'
+
+const Header = ({ children }) => {
+  return (
+    <div className="pf-chatbot__header">
+      {children}
+    </div>
+  )
+}
+
+export default Header
