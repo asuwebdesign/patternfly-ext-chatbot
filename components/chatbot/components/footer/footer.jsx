6055aee63b586a25fc9d0148cd15311f2c30348d
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-11 09:01:28 -0400

    Setup footer region

--- components/chatbot/components/footer/footer.jsx
+++ components/chatbot/components/footer/footer.jsx
@@ -1,5 +1,22 @@
+// ============================================================================
+// Chatbot Footer
+// ============================================================================
+
 // Global footer component
 // Supports:
 // - Message bar
 // - Footnote
 // - Footnote popover
+
+// Import styles
+import './Footer.scss'
+
+const Footer = ({ children }) => {
+  return (
+    <div className="pf-chatbot__footer">
+      {children}
+    </div>
+  )
+}
+
+export default Footer
