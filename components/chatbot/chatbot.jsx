1e8f722617500bbf348b3929e2761c492f0659dc
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-11 08:57:59 -0400

    Setup chatbot

--- components/chatbot/chatbot.jsx
+++ components/chatbot/chatbot.jsx
@@ -1,38 +1,47 @@
-{/*
-  Splash
-  - Loading timeout */}
+// ============================================================================
+// Chatbot
+// ============================================================================
+import React from 'react'
+import { motion } from 'framer-motion'
 
-{/*
-  Onboarding + New features
-  - Skip URL
-  - Illustration SRC
-  - Title string
-  - Description string
-  - Continue URL
-  - Pagination Active */}
+// Import Chatbot components
+import Chat from './layouts/Chat/Chat'
+import DeclinedLegal from './layouts/DeclinedLegal/DeclinedLegal'
+import Onboarding from './layouts/Onboarding/Onboarding'
+import PrivacyStatement from './layouts/PrivacyStatement/PrivacyStatement'
+import Splash from './layouts/Splash/Splash'
+import TermsOfUse from './layouts/TermsOfUse/TermsOfUse'
 
-{/*
-  EULA
-  - Illustration SRC
-  - Title string
-  - Description string
-  - Continue URL
-  - Footnote string */}
+// Import styles
+import './Chatbot.scss'
 
-{/*
-  Chatbot header
-  - Logo SRC or Title string
-  - Menu booleon
-  - Menu active state
-  - Options booleon
-  - Options active state */}
+const Chatbot = ({ displayMode, layout, chatbotVisible, ...props }) => {
 
-{/*
-  Chatbot footer
-  - Message contents
-  - Attachment booleon
-  - Microphone booleon
-  - Footnote string
-  - Footnote popover */}
+  // Configure animations
+  const motionChatbot = {
+    open: { opacity: 1, y: 0 },
+    closed: { opacity: 0, y: '16px' }
+  }
 
-<div className="pf-chatbot" lang="en"></div>
+  return (
+    <motion.div
+      className={`pf-chatbot ${layout && 'pf-chatbot--layout--' + layout} ${displayMode} ${!chatbotVisible ? 'pf-chatbot--hidden' : ''}`}
+      variants={motionChatbot}
+      initial="closed"
+      animate={chatbotVisible ? "open" : "closed"}
+    >
+      
+      {layout === 'splash' && <Splash {...props} />}
+      {layout === 'terms-of-use' && <TermsOfUse {...props} />}
+      {layout === 'privacy-statement' && <PrivacyStatement {...props} />}
+      {layout === 'declined-legal' && <DeclinedLegal {...props} />}
+      {layout === 'onboarding' && <Onboarding {...props} />}
+      {layout === 'chat' && <Chat {...props} />}
+
+      {/* Or add a custom layout... */}
+
+    </motion.div>
+  )
+}
+
+export default Chatbot
