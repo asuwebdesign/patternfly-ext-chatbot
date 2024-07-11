2bc1791cf1f9b12690c7500e501221f40f8322f6
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-11 08:59:26 -0400

    Init video messages

---
+++ components/chatbot/components/Main/Message/content/VideoMessage/VideoMessage.jsx
@@ -0,0 +1,14 @@
+// ============================================================================
+// Chatbot Main - Message - Content - Video
+// ============================================================================
+
+// Suggest using VidStack
+import React from 'react'
+
+const VideoMessage = () => {
+  return (
+    <pre>Inline code</pre>
+  )
+}
+
+export default VideoMessage
