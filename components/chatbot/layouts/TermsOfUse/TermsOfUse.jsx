771c1079341c5b8e10d514da23e8be4b1790cbd8
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-11 08:54:27 -0400

    Setup terms of use screen

---
+++ components/chatbot/layouts/TermsOfUse/TermsOfUse.jsx
@@ -0,0 +1,41 @@
+// ============================================================================
+// Chatbot Layout - Terms of Use
+// ============================================================================
+
+import React from 'react'
+
+// Import PatternFly components
+import { Button, Text, TextVariants } from '@patternfly/react-core'
+
+// Import Chatbot components
+import Header from '../../components/Header/Header'
+import Main from '../../components/Main/Main'
+import Footer from '../../components/Footer/Footer'
+import Illustration from './Illustration'
+
+// Import styles
+import './TermsOfUse.scss'
+
+const TermsOfUse = ({ termsOfUseTitle, termsOfUseAgreement }) => {
+
+  const title = termsOfUseTitle ? termsOfUseTitle : 'Terms of use'
+  const content = termsOfUseAgreement ? termsOfUseAgreement : 'No agreement has been provided'
+
+  return (
+    <>
+      <Header>
+        <div className="pf-chatbot__illustration">
+          <Illustration />
+        </div>
+        <Text component={TextVariants.h1}>{title}</Text>
+      </Header>
+      <Main>{content}</Main>
+      <Footer>
+        <Button variant="tertiary">Decline</Button>
+        <Button>Accept</Button>
+      </Footer>
+    </>
+  )
+}
+
+export default TermsOfUse
