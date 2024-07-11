2e82eb0b3148ddca16952f194c270d0a9f93af92
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-11 09:02:45 -0400

    Init footnote

--- components/chatbot/components/footer/footnote/footnote.jsx
+++ components/chatbot/components/footer/footnote/footnote.jsx
@@ -1,1 +1,30 @@
-// Footnote - displayed in the footer
+// ============================================================================
+// Chatbot Footer - Footnote
+// ============================================================================
+
+import React from 'react'
+
+// Import Patternfly components
+import { Popover, Button } from '@patternfly/react-core'
+
+// Import styles
+import './Footnote.scss'
+
+const Footnote = () => {
+
+  // Optional. Upgrade to text. Upgrade to popover.
+
+  // Footnote popover
+  // - image
+  // - heading
+  // - desc
+  // - got it button (dismiss)
+  // - ext link text
+  // - ext link url
+
+  return (
+    <div>Footnote goes here</div>
+  )
+}
+
+export default Footnote
