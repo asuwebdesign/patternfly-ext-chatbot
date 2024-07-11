18e5e776a0245230d3dedef1c3e596b21282dc60
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-11 08:59:59 -0400

    Setup main region

---
+++ components/chatbot/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+// ============================================================================
+// Chatbot Main
+// ============================================================================
+
+// Import styles
+import './Main.scss'
+
+const Main = ({ children }) => {
+  return (
+    <div className="pf-chatbot__main">
+      {children}
+    </div>
+  )
+}
+
+export default Main
