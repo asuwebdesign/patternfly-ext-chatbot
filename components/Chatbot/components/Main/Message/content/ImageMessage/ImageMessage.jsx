8bd622df9ecc450df31b04527a36a604f6e5b571
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-12 09:06:46 -0400

    Setup props for ImageMessage

--- components/Chatbot/components/Main/Message/content/ImageMessage/ImageMessage.jsx
+++ components/Chatbot/components/Main/Message/content/ImageMessage/ImageMessage.jsx
@@ -7,11 +7,7 @@ import React from 'react'
 // Import styles
 import './ImageMessage.scss'
 
-const ImageMessage = () => {
-
-  const imageSrc = 'https://cdn.dribbble.com/userupload/15532609/file/original-e98aacdfdd1aad5b1a07d597e7771735.jpg?resize=2048x1536'
-  const imageAlt = ''
-
+const ImageMessage = ({ imageSrc, imageAlt }) => {
   return (
     <div className="pf-chatbot__message-image">
       <img src={imageSrc} alt={imageAlt} />
