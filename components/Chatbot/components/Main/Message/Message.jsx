1ffdb332f5b5b0e050188e72a4b9f217c8e334f6
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-12 09:07:27 -0400

    Tested sample content for Message

--- components/Chatbot/components/Main/Message/Message.jsx
+++ components/Chatbot/components/Main/Message/Message.jsx
@@ -28,6 +28,14 @@ const Message = () => {
 
   const handleMessage = () => {}
 
+  // ---- Sample content ----
+  const sampleText = <>By default <code>{'push()'}</code></>
+  const sampleList = ['Item 1', 'Item 2', 'Item 3']
+  const sampleListOrdered = ['Item 1', 'Item 2', 'Item 3']
+  const sampleImgSrc = 'https://cdn.dribbble.com/userupload/15532609/file/original-e98aacdfdd1aad5b1a07d597e7771735.jpg?resize=2048x1536'
+  const sampleImgAlt = 'Mockup of 3 iPhones on blue background'
+  // ---- Sample content ----
+
   return (
     <div className="pf-chatbot__message">
       <Avatar src={userAvatarSrc} alt={userAvatarAlt} />
@@ -37,9 +45,10 @@ const Message = () => {
           <time dateTime="">{timestamp}</time>
         </div>
         <div className="pf-chatbot__message-response">
-          <TextMessage />
-          <ListMessage />
-          <ImageMessage />
+          <TextMessage text={sampleText} />
+          <ListMessage items={sampleList} />
+          <ListMessage ordered items={sampleListOrdered} />
+          <ImageMessage imageSrc={sampleImgSrc} imageAlt={sampleImgAlt} />
         </div>
         <Sources />
       </div>
