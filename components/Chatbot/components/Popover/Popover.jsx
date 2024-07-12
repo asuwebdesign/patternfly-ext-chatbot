a0f8c6a66326602258a7aae4171ec6d89d9ec4ca
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-12 08:36:20 -0400

    Extended popover component

--- components/Chatbot/components/Popover/Popover.jsx
+++ components/Chatbot/components/Popover/Popover.jsx
@@ -8,10 +8,10 @@ import { Popover as PatternflyPopover } from '@patternfly/react-core'
 // Import styles
 import './Popover.scss'
 
-const Popover = ({ children, ...props }) => {
+const Popover = ({ children, className, ...props }) => {
   return (
     <>
-      <PatternflyPopover className="pf-chatbot__popover" {...props}>
+      <PatternflyPopover className={`pf-chatbot__popover ${className}`} {...props}>
         {children}
       </PatternflyPopover>
     </>
