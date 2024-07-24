de591fdaac0d3b0aac00b53d78d2fbdc933163c2
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-24 08:20:54 -0400

    Update Brand default values

--- components/Chatbot/components/Brand/Brand.jsx
+++ components/Chatbot/components/Brand/Brand.jsx
@@ -14,11 +14,17 @@ import './Brand.scss'
 
 const Brand = ({ config={} }) => {
 
+  // Configure default values
   const {
     brand = {
-      srcLight: 'https://staging-v6.patternfly.org/images/f18506f4.svg',
-      srcDark: 'https://staging-v6.patternfly.org/images/ca43ebed.svg',
-      alt: 'PatternFly Logo'
+      lightTheme: {
+        src: 'https://staging-v6.patternfly.org/images/f18506f4.svg',
+        alt: 'PatternFly Logo'
+      },
+      darkTheme: {
+        src: 'https://staging-v6.patternfly.org/images/ca43ebed.svg',
+        alt: 'PatternFly Logo'
+      }
     }
   } = config.global || {}
 
@@ -26,7 +32,7 @@ const Brand = ({ config={} }) => {
   const isDarkMode = useDarkMode()
 
   return (
-    <PatternflyBrand src={isDarkMode ? brand.srcDark : brand.srcLight} alt={brand.alt} />
+    <PatternflyBrand src={isDarkMode ? brand.darkTheme.src : brand.lightTheme.src} alt={isDarkMode ? brand.darkTheme.alt : brand.lightTheme.alt} />
   )
 }
 
