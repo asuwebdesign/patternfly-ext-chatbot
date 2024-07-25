8eb0677f6b040604b2fb265c947106eb66c9ae7d
Mark Riggan <5150241+asuwebdesign@users.noreply.github.com>
2024-07-25 08:20:35 -0400

    Update Menu item placeholders

--- components/Chatbot/components/Menu/Menu.jsx
+++ components/Chatbot/components/Menu/Menu.jsx
@@ -64,79 +64,100 @@ const Menu = ({ config, displayMode, isMenuExpanded, setIsMenuExpanded, onMenuEx
       </NavGroup>
       <NavGroup title="Yesterday">
         <NavItem preventDefault itemId="group-2_item-1" isActive={activeItem === 'group-2_item-1'}>
-          Group 2 Link 1
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Red Hat products and services" />
         </NavItem>
         <NavItem preventDefault itemId="group-2_item-2" isActive={activeItem === 'group-2_item-2'}>
-          Group 2 Link 2
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Enterprise Linux installation and setup" />
         </NavItem>
         <NavItem preventDefault itemId="group-2_item-3" isActive={activeItem === 'group-2_item-3'}>
-          Group 2 Link 3
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Troubleshoot system crash" />
         </NavItem>
       </NavGroup>
       <NavGroup title="June">
-        <NavItem preventDefault to="#nav-group2-item1" itemId="group-2_item-1" isActive={activeItem === 'group-2_item-1'}>
-          Group 3 Link 1
+        <NavItem preventDefault itemId="group-3_item-1" isActive={activeItem === 'group-3_item-1'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Red Hat products and services" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item2" itemId="group-2_item-2" isActive={activeItem === 'group-2_item-2'}>
-          Group 3 Link 2
+        <NavItem preventDefault itemId="group-3_item-2" isActive={activeItem === 'group-3_item-2'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Enterprise Linux installation and setup" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item3" itemId="group-2_item-3" isActive={activeItem === 'group-2_item-3'}>
-          Group 3 Link 3
+        <NavItem preventDefault itemId="group-3_item-3" isActive={activeItem === 'group-3_item-3'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Troubleshoot system crash" />
         </NavItem>
       </NavGroup>
       <NavGroup title="May">
-        <NavItem preventDefault to="#nav-group2-item1" itemId="group-2_item-1" isActive={activeItem === 'group-2_item-1'}>
-          Group 4 Link 1
+        <NavItem preventDefault itemId="group-4_item-1" isActive={activeItem === 'group-4_item-1'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Red Hat products and services" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item2" itemId="group-2_item-2" isActive={activeItem === 'group-2_item-2'}>
-          Group 4 Link 2
+        <NavItem preventDefault itemId="group-4_item-2" isActive={activeItem === 'group-4_item-2'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Enterprise Linux installation and setup" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item3" itemId="group-2_item-3" isActive={activeItem === 'group-2_item-3'}>
-          Group 4 Link 3
+        <NavItem preventDefault itemId="group-4_item-3" isActive={activeItem === 'group-4_item-3'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Troubleshoot system crash" />
         </NavItem>
       </NavGroup>
       <NavGroup title="April">
-        <NavItem preventDefault to="#nav-group2-item1" itemId="group-2_item-1" isActive={activeItem === 'group-2_item-1'}>
-          Group 5 Link 1
+        <NavItem preventDefault itemId="group-5_item-1" isActive={activeItem === 'group-5_item-1'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Red Hat products and services" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item2" itemId="group-2_item-2" isActive={activeItem === 'group-2_item-2'}>
-          Group 5 Link 2
+        <NavItem preventDefault itemId="group-5_item-2" isActive={activeItem === 'group-5_item-2'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Enterprise Linux installation and setup" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item3" itemId="group-2_item-3" isActive={activeItem === 'group-2_item-3'}>
-          Group 5 Link 3
+        <NavItem preventDefault itemId="group-5_item-3" isActive={activeItem === 'group-5_item-3'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Troubleshoot system crash" />
         </NavItem>
       </NavGroup>
       <NavGroup title="March">
-        <NavItem preventDefault to="#nav-group2-item1" itemId="group-2_item-1" isActive={activeItem === 'group-2_item-1'}>
-          Group 3 Link 1
+        <NavItem preventDefault itemId="group-6_item-1" isActive={activeItem === 'group-6_item-1'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Red Hat products and services" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item2" itemId="group-2_item-2" isActive={activeItem === 'group-2_item-2'}>
-          Group 3 Link 2
+        <NavItem preventDefault itemId="group-6_item-2" isActive={activeItem === 'group-6_item-2'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Enterprise Linux installation and setup" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item3" itemId="group-2_item-3" isActive={activeItem === 'group-2_item-3'}>
-          Group 3 Link 3
+        <NavItem preventDefault itemId="group-6_item-3" isActive={activeItem === 'group-6_item-3'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Troubleshoot system crash" />
         </NavItem>
       </NavGroup>
       <NavGroup title="February">
-        <NavItem preventDefault to="#nav-group2-item1" itemId="group-2_item-1" isActive={activeItem === 'group-2_item-1'}>
-          Group 4 Link 1
+        <NavItem preventDefault itemId="group-7_item-1" isActive={activeItem === 'group-7_item-1'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Red Hat products and services" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item2" itemId="group-2_item-2" isActive={activeItem === 'group-2_item-2'}>
-          Group 4 Link 2
+        <NavItem preventDefault itemId="group-7_item-2" isActive={activeItem === 'group-7_item-2'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Enterprise Linux installation and setup" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item3" itemId="group-2_item-3" isActive={activeItem === 'group-2_item-3'}>
-          Group 4 Link 3
+        <NavItem preventDefault itemId="group-7_item-3" isActive={activeItem === 'group-7_item-3'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Troubleshoot system crash" />
         </NavItem>
       </NavGroup>
       <NavGroup title="January">
-        <NavItem preventDefault to="#nav-group2-item1" itemId="group-2_item-1" isActive={activeItem === 'group-2_item-1'}>
-          Group 5 Link 1
+        <NavItem preventDefault itemId="group-8_item-1" isActive={activeItem === 'group-8_item-1'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Red Hat products and services" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item2" itemId="group-2_item-2" isActive={activeItem === 'group-2_item-2'}>
-          Group 5 Link 2
+        <NavItem preventDefault itemId="group-8_item-2" isActive={activeItem === 'group-8_item-2'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Enterprise Linux installation and setup" />
         </NavItem>
-        <NavItem preventDefault to="#nav-group2-item3" itemId="group-2_item-3" isActive={activeItem === 'group-2_item-3'}>
-          Group 5 Link 3
+        <NavItem preventDefault itemId="group-8_item-3" isActive={activeItem === 'group-8_item-3'}>
+          <FontAwesomeIcon icon={faMessage} />
+          <Truncate content="Troubleshoot system crash" />
         </NavItem>
       </NavGroup>
     </Nav>
